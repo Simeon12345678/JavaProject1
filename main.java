8fd74b9a1a91e937c47e361b8c882f0b04c24eb5
Simeon12345678 <simeon.milic@elev.ga.ntig.se>
2024-01-22 08:48:43 +0100

    first commit

---
+++ main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+
+    int playerHP;
+    int playerATK;
+    String[] weapons = {"Sword", "Axe", "Bow"};
+
+    int enemyHP;
+    int enemyATK;
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+    }
+}
